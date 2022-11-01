211abee5512b7af2ed686710a9924753fa4a5031
Jackson-Wozniak <jjw30@comcast.net>
2022-11-01 12:04:30 -0400

    rename src file to Backend

--- Backend/main/java/org/example/flight_sim_career_mode/flightgenerator/FlightController.java
+++ Backend/main/java/org/example/flight_sim_career_mode/flightgenerator/FlightController.java
@@ -2,6 +2,7 @@ package org.example.flight_sim_career_mode.flightgenerator;
 
 import lombok.AllArgsConstructor;
 import org.example.flight_sim_career_mode.pilot.model.entity.Pilot;
+import org.example.flight_sim_career_mode.pilot.service.PilotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,14 @@ public class FlightController {
 
     @Autowired
     private final GenerateFlight generateFlight;
-
     @Autowired
     private final FlightService flightService;
+    @Autowired
+    private final PilotService pilotService;
 
     @RequestMapping(value = "/get")
     public Flight getRandomFlight(){
-        Flight flight = generateFlight.createFlight(new Pilot("Name", "US"));
+        Flight flight = generateFlight.createFlight(pilotService.findPilotByName("Name"));
         flightService.saveFlight(flight);
         return flight;
     }
